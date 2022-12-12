8104f9b8d389cb1029cf8a618172bedf76691012
Erick Segura <gestionecesaria@gmail.com>
2022-12-12 11:09:58 -0400

    JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto = {
+   nombre: nombre,
+   edad: edad,
+   meow: function habla() {
+         return 'Meow!'
+   },
+  }
+  return objeto;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +21,8 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+ objeto[property] = null;
+ return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,13 +30,15 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]()
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+var resul = objetoMisterioso['numeroMisterioso'] * 5;
+return resul;
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
@@ -34,6 +46,8 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad]
+  return objeto
 }
 
 function nuevoUsuario (nombre, email, password) {
